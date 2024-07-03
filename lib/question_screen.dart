c69be1abbfd1c025a962cc09e6657e38d5c364e4
Chandan kumar <chandandpu@gmail.com>
2024-07-03 19:25:54 +0530

    add firebase

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,11 +1,11 @@
 import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
 import 'dart:async' show Future, Timer;
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter/widgets.dart';
+import 'package:sscprevious/widget/question_information_container.dart';
 class QuestionScreen extends StatefulWidget {
   String type;
   String file;
@@ -36,6 +36,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
   Random random =Random();
   int index=1;
   int _start=1;
+  bool _isLoading = false;
   String _explanation='';
   List<String> option=[];
   DateTime date=DateTime.timestamp();
@@ -43,18 +44,29 @@ class _QuestionScreenState extends State<QuestionScreen> {
   @override
   void initState() {
     super.initState();
-    _startTimer();
     loadCSV();
+    // getQuestionFile();
     print(widget.file);
   }
-  _changeQuestion(){
+  _changeQuestion(int a){
     setState(() {
       try{
-        if (index == data.length - 1) {
-          index = 0;
+        // if (index>data.length - 1) {
+        //   index = 0;
+        //   _start=1;
+        // }
+        // if(_start<1){
+        //   _start=data.length-1;
+        //   index=data.length-1;
+        // }
+        index = index+a;
+        _start=_start+a;
+        if(_start>data.length-1){
+          _start=1;
         }
-        index = random.nextInt(data.length) + 1;
-        _start++;
+        if(_start<1){
+          _start=data.length-1;
+        }
         shortQuestion = data[index][8].toString();
         questionHeader=data[index][9].toString();
         option_a = data[index][1].toString();
@@ -69,6 +81,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
         _colorContainerB = Colors.white;
         _colorContainerC = Colors.white;
         flag = false;
+        // QuestionInformation(flag: flag,start: _start, isLoading: _isLoading, answer: answer, explanation: _explanation, option_a: option_a, option_b: option_b, option_c: option_c, option_d: option_d, question_header: questionHeader, shortQuestion: shortQuestion, year: year);
       }
       catch(e){
         print("Error Comes ${e.toString()}");
@@ -87,24 +100,57 @@ class _QuestionScreenState extends State<QuestionScreen> {
       });
     });
   }
+  Future<void> getQuestionFile() async{
+    try{
+      FirebaseStorage storage=FirebaseStorage.instance;
+      Reference ref=storage.ref().child("spottingError.csv");
+      String downloadUrl=await ref.getDownloadURL();
+      print("DownloadLink ${downloadUrl}");
+      final response= await http.get(Uri.parse(downloadUrl));
+      if(response.statusCode==200){
+        List<List<dynamic>> table=CsvToListConverter().convert(response.body);
+        print(table.length);
+        print("Tbale Data ${table}");
+      }else{
+        print(response.statusCode);
+      }
+    }catch(e){
+      print(e.toString());
+    }
+  }
   Future<void> loadCSV() async {
     try{
-      final String rawCSV = await rootBundle.loadString("assets/spottingError.csv");
-      List<List<dynamic>> csvTable = CsvToListConverter().convert(rawCSV);
+      FirebaseStorage storage=FirebaseStorage.instance;
+      Reference ref=storage.ref().child("spottingError.csv");
+      String downloadUrl=await ref.getDownloadURL();
+      final response= await http.get(Uri.parse(downloadUrl));
       setState(() {
-        data = csvTable;
-        // List<dynamic> row=data[1];
-        index=random.nextInt(data.length-1);
-        shortQuestion = data[index][8].toString();
-        questionHeader=data[index][9].toString();
-        option_a=data[index][1].toString();
-        option_b=data[index][2].toString();
-        option_c=data[index][3].toString();
-        option_d=data[index][4].toString();
-        answer=data[index][6].toString();
-        year=data[index][5].toString();
-        _explanation=data[index][7].toString();
+        _isLoading=false;
       });
+      if(response.statusCode==200) {
+        List<List<dynamic>> table = CsvToListConverter().convert(response.body);
+        // print(table.length);
+        // print("Tbale Data ${table}");
+        // final String rawCSV = await rootBundle.loadString("assets/spottingError.csv");
+        // List<List<dynamic>> csvTable = CsvToListConverter().convert(rawCSV);
+        // print(csvTable.length);
+        setState(() {
+          data = table;
+          // List<dynamic> row=data[1];
+          index = random.nextInt(data.length - 1);
+          shortQuestion = data[index][8].toString();
+          questionHeader = data[index][9].toString();
+          option_a = data[index][1].toString();
+          option_b = data[index][2].toString();
+          option_c = data[index][3].toString();
+          option_d = data[index][4].toString();
+          answer = data[index][6].toString();
+          year = data[index][5].toString();
+          _explanation = data[index][7].toString();
+          _isLoading=true;
+          _startTimer();
+        });
+      }
     }catch(e){
       print("Errors Comes ${e.toString()}");
     }
@@ -123,6 +169,37 @@ class _QuestionScreenState extends State<QuestionScreen> {
         automaticallyImplyLeading: false,
         leading: Center(child: Text("${_minutes<10?"0${_minutes}":_minutes}:${_second<10?"0${_second}":_second}",style: TextStyle(color: Colors.red,fontWeight: FontWeight.w500),)),
         title: Center(child: Text(widget.type)),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green,
+        child: Container(
+          child:
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: (){
+                    _changeQuestion(-1);
+                  },
+                  child: Container(
+                    child: Center(child: Text("Previous",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500))),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: (){
+                    _changeQuestion(1);
+                  },
+                  child: Container(
+                    child: Center(child: Text("Next",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500))),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: WillPopScope(
         onWillPop:  () async {
@@ -152,158 +229,16 @@ class _QuestionScreenState extends State<QuestionScreen> {
         child: LayoutBuilder(
             builder: (context,constraints){
               if(constraints.maxWidth<600){
-                return SingleChildScrollView(
-                  child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Card(
-                          elevation: 5,
-                          shadowColor: Colors.black,
-                          color: Colors.white,
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                width: double.infinity,
-                                margin: EdgeInsets.all(5),
-                                decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black.withOpacity(.7)),
-                                  borderRadius: BorderRadius.circular(5),
-                                ),
-                                padding:EdgeInsets.all(5),
-                                child:Center(child: Text(questionHeader,style: TextStyle(color: Colors.black.withOpacity(.7),fontSize: 14),)),
-                              ),
-                              SizedBox(height: 5,),
-                              Container(alignment: Alignment.topLeft,margin:EdgeInsets.all(5),child: Text("${_start}. "+shortQuestion,style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),)),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Expanded(
-                                    child: Container(
-                                      alignment: AlignmentDirectional.centerEnd,
-                                        padding: EdgeInsets.all(5),
-                                        child: Text(year,style: TextStyle(fontSize: 12,fontWeight: FontWeight.w600,color: Colors.black.withOpacity(.8)),),
-                                      ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 5,),
-
-                              Row(
-                                children: [Expanded(
-                                    child: GestureDetector(
-                                      onTap: (){
-                                        _OptionAContainer("a");
-                                      },
-                                      child: Container(
-                                        margin: EdgeInsets.all(8),
-                                        padding: EdgeInsets.all(8),
-                                        child: Text("A) "+option_a,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
-                                        decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(10),
-                                            border: Border.all(color: Colors.black.withOpacity(.3)),
-                                            color: _colorContainerA
-                                        ),
-                                      ),
-                                    )
-                                  ),
-                                ]
-                              ),
-                              Row(
-                                children: [
-                                  Expanded(
-                                      child: GestureDetector(
-                                        onTap: (){
-                                          _OptionBContainer("b");
-                                        },
-                                        child: Container(
-                                          margin: EdgeInsets.all(8),
-                                          padding: EdgeInsets.all(8),
-                                          child: Text("B) "+option_b,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(color: Colors.black.withOpacity(.3)),
-                                              borderRadius: BorderRadius.circular(10),
-                                              color:_colorContainerB
-                                          ),
-                                        ),
-                                      )
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Expanded(
-                                      child: GestureDetector(
-                                        onTap: (){
-                                          _OptionCContainer("c");
-                                        },
-                                        child: Container(
-
-                                          margin: EdgeInsets.all(8),
-                                          padding: EdgeInsets.all(8),
-                                          child: Text("C) "+option_c,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(color: Colors.black.withOpacity(.3)),
-                                              borderRadius: BorderRadius.circular(10),
-                                              color: _colorContainerC
-                                          ),
-                                        ),
-                                      )
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Expanded(
-                                      child: GestureDetector(
-                                        onTap: (){
-                                          _OptionDContainer("d");
-                                        },
-                                        child: Container(
-                                          margin: EdgeInsets.all(8),
-                                          padding: EdgeInsets.all(8),
-                                          child: Text("D) "+option_d,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(color: Colors.black.withOpacity(.3)),
-                                              borderRadius: BorderRadius.circular(10),
-                                              color: _colorContainerD
-                                          ),
-                                        ),
-                                      )
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 8,),
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: GestureDetector(
-                                      onTap: (){
-                                        _changeQuestion();
-                                      },
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(10),
-                                            color: Colors.green
-                                        ),
-                                        margin: EdgeInsets.all(8),
-                                        padding: EdgeInsets.all(10),
-                                        child: Center(child: Text("Next",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500))),
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              flag ? Container(
-                                margin: EdgeInsets.all(10),
-                                padding: EdgeInsets.all(5),
-                                child: Center(child: Text(_explanation,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: Colors.black.withOpacity(.9)),)),
-                              ):SizedBox(),
-                            ],
-                          ),
+                return _isLoading ? SingleChildScrollView(
+                  child: Column(
+                          children: [
+                            QuestionInformation(colorContainerA: _colorContainerA,colorContainerB: _colorContainerB
+                                ,colorContainerC: _colorContainerC,colorContainerD: _colorContainerD
+                                ,OptionAContainer: _OptionAContainer,OptionBContainer: _OptionBContainer,OptionCContainer: _OptionCContainer
+                                ,OptionDContainer: _OptionDContainer,flag: flag,start: _start, isLoading: _isLoading, answer: answer, explanation: _explanation, option_a: option_a, option_b: option_b, option_c: option_c, option_d: option_d, question_header: questionHeader, shortQuestion: shortQuestion, year: year),
+                          ],
                         ),
-                      ),
-                  ),
-                );
+                ): Center(child:CircularProgressIndicator());
               }else{
                 return SingleChildScrollView(
                   child: Center(
@@ -479,7 +414,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
                                 Expanded(
                                   child: GestureDetector(
                                     onTap: (){
-                                      _changeQuestion();
+                                      _changeQuestion(1);
                                     },
                                     child: Container(
                                       decoration: BoxDecoration(
--- lib/widget/question_information_container.dart
+++ lib/widget/question_information_container.dart
@@ -1,6 +1,29 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
 class QuestionInformation extends StatefulWidget {
-  const QuestionInformation({super.key});
+  String question_header;
+  String shortQuestion;
+  String option_a;
+  String option_b;
+  String option_c;
+  String option_d;
+  final void Function(String) OptionAContainer;
+  final Function(String) OptionBContainer;
+  final Function(String) OptionCContainer;
+  final Function(String) OptionDContainer;
+  final colorContainerA;
+  final colorContainerB;
+  final colorContainerC;
+  final colorContainerD;
+  String year;
+  String explanation;
+  bool isLoading;
+  String answer;
+  int start;
+  bool flag;
+  QuestionInformation({super.key ,required this.colorContainerB, required this.colorContainerC,required this.colorContainerD,required this.OptionAContainer,
+    required this.OptionBContainer,required this.OptionCContainer,required this.OptionDContainer, required this.colorContainerA,required this.flag,required this.start, required this.isLoading,required this.answer,required this.explanation,required this.option_a,required this.option_b,required this.option_c,
+  required this.option_d,required this.question_header,required this.shortQuestion,required this.year});
 
   @override
   State<QuestionInformation> createState() => _QuestionInformationState();
@@ -9,6 +32,138 @@ class QuestionInformation extends StatefulWidget {
 class _QuestionInformationState extends State<QuestionInformation> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Card(
+                elevation: 5,
+                shadowColor: Colors.black,
+                color: Colors.white,
+                child: widget.isLoading ? Column(
+                  children: <Widget>[
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black.withOpacity(.7)),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding:EdgeInsets.all(5),
+                      child:Center(child: Text(widget.question_header,style: TextStyle(color: Colors.black.withOpacity(.7),fontSize: 14),)),
+                    ),
+                    SizedBox(height: 5,),
+                    Container(alignment: Alignment.topLeft,margin:EdgeInsets.all(5),child: Text("${widget.start}. "+widget.shortQuestion,style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            alignment: AlignmentDirectional.centerEnd,
+                            padding: EdgeInsets.all(5),
+                            child: Text(widget.year,style: TextStyle(fontSize: 12,fontWeight: FontWeight.w600,color: Colors.black.withOpacity(.8)),),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5,),
+                    Row(
+                        children: [Expanded(
+                            child: GestureDetector(
+                              onTap: (){
+                                widget.OptionAContainer("a");
+                              },
+                              child: Container(
+                                margin: EdgeInsets.all(8),
+                                padding: EdgeInsets.all(8),
+                                child: Text("A) "+widget.option_a,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(color: Colors.black.withOpacity(.3)),
+                                    color: widget.colorContainerA
+                                ),
+                              ),
+                            )
+                        ),
+                        ]
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: GestureDetector(
+                              onTap: (){
+                                widget.OptionBContainer("b");
+                              },
+                              child: Container(
+                                margin: EdgeInsets.all(8),
+                                padding: EdgeInsets.all(8),
+                                child: Text("B) "+widget.option_b,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black.withOpacity(.3)),
+                                    borderRadius: BorderRadius.circular(10),
+                                    color:widget.colorContainerB
+                                ),
+                              ),
+                            )
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: GestureDetector(
+                              onTap: (){
+                                widget.OptionCContainer("c");
+                              },
+                              child: Container(
+      
+                                margin: EdgeInsets.all(8),
+                                padding: EdgeInsets.all(8),
+                                child: Text("C) "+widget.option_c,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black.withOpacity(.3)),
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: widget.colorContainerC
+                                ),
+                              ),
+                            )
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: GestureDetector(
+                              onTap: (){
+                                widget.OptionDContainer("d");
+                              },
+                              child: Container(
+                                margin: EdgeInsets.all(8),
+                                padding: EdgeInsets.all(8),
+                                child: Text("D) "+widget.option_d,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black.withOpacity(.3)),
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: widget.colorContainerD
+                                ),
+                              ),
+                            )
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8,),
+                    widget.flag ? Column(
+                      children: [
+                        Container(height: 1,color: Colors.black,margin: EdgeInsets.only(left: 2,right: 2),),
+                        SizedBox(height: 2,),
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(5),
+                          child: Center(child: Text(widget.explanation,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: Colors.black.withOpacity(.9)),)),
+                        ),
+                      ],
+                    ):SizedBox(),
+                  ],
+                ):CircularProgressIndicator()
+      
+      ),
+    );
   }
 }
